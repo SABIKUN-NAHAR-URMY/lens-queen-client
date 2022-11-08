3033a561d8cdcaad89eb8c4adf13ef1ae39182b0
SABIKUN-NAHAR-URMY <snurmy18@gmail.com>
2022-11-08 18:27:13 +0600

    footer section done

--- src/pages/Footer/Footer.js
+++ src/pages/Footer/Footer.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import logo from '../../images/logo.png';
 
 const Footer = () => {
     return (
-        <div>
-            
-        </div>
+        <footer className="footer items-center mt-14 p-4 bg-black text-white">
+            <div className="items-center grid-flow-col">
+                <img className='w-32' src={logo} alt="" />
+                <p>Copyright © 2022 - All right reserved</p>
+            </div>
+            <div className="footer p-10 ">
+                <div>
+                    <span className="footer-title">Services</span>
+                    <a className="link link-hover">Branding</a>
+                    <a className="link link-hover">Design</a>
+                    <a className="link link-hover">Marketing</a>
+                    <a className="link link-hover">Advertisement</a>
+                </div>
+                <div>
+                    <span className="footer-title">Company</span>
+                    <a className="link link-hover">About us</a>
+                    <a className="link link-hover">Contact</a>
+                    <a className="link link-hover">Jobs</a>
+                    <a className="link link-hover">Press kit</a>
+                </div>
+            </div>
+        </footer>
     );
 };
 
