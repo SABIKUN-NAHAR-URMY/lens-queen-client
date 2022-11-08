3942e2657842e80b366349d36c9d1b66b407efdb
SABIKUN-NAHAR-URMY <snurmy18@gmail.com>
2022-11-08 16:08:35 +0600

    slider update

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 1. React-router-dom
 2. TailwindCSS and DaisyUi
 3. 
+# This is my project wedding photography related and named LENS-QUEEN
--- src/pages/Home/Slider/Slider.js
+++ src/pages/Home/Slider/Slider.js
@@ -9,13 +9,15 @@ const Slider = ({ slider }) => {
                 <img className="w-full" src={image} alt="" />
             </div>
 
-            <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
+            <div className="absolute flex justify-center transform -translate-y-1/2 left-5 right-5 bottom-0">
                 <a href={`#slide${prev}`} className="btn btn-circle mr-5">❮</a>
                 <a href={`#slide${next}`} className="btn btn-circle">❯</a>
             </div>
-            <div className="absolute flex justify-between transform -translate-y-1/2 top-1/2 pl-11">
-                <h1 className='text-6xl text-white font-bold'>Plan and book <br />
-                    your wedding</h1>
+            <div className="absolute top-1/2 left-52">
+                <h1 className='text-6xl text-white font-bold'>The Most Important Thing <br />
+                At a Wedding is to <br />
+                Photography...
+                </h1>
             </div>
         </div>
 
