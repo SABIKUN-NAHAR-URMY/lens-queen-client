02aa34ee2d1ac369d76fcf6b816462f1c50f3872
SABIKUN-NAHAR-URMY <snurmy18@gmail.com>
2022-11-09 18:14:51 +0600

    my review section update

--- src/pages/MyReviews/MyReviews.js
+++ src/pages/MyReviews/MyReviews.js
@@ -11,13 +11,18 @@ const MyReviews = () => {
             .then(res => res.json())
             .then(data => setReviewMy(data))
     }, [user?.email]);
-
+    console.log(reviewMy)
     return (
         <div>
             {
-                reviewMy.map(rm => <MyReviewShow
-                key={rm._id}
-                rm={rm}></MyReviewShow>)
+                reviewMy.length === 0 ?
+                    <h1 className='text-5xl font-bold text-center m-11'>No Review added!</h1>
+                    :
+
+                    reviewMy.map(rm => <MyReviewShow
+                        key={rm._id}
+                        rm={rm}></MyReviewShow>)
+
             }
         </div>
     );
